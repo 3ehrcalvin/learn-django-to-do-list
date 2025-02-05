3f1d0e7640a37ab9bfc670e0a034def1969f1fdd
3ehrcalvin <calvin@3ecpa.com.sg>
2025-02-05 18:03:49 +0800

    feat: add update function

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("list/", views.home, name="list"),
     path("form/", views.list_form, name="form"),
+    path("form/<int:rec_id>", views.list_form_update, name="form"),
     path("form/submit", views.submit_form, name="submit"),
     path("delete/<int:rec_id>", views.delete_list, name="delete"),
+    path("done/<int:rec_id>", views.record_is_done, name="done"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -16,20 +16,35 @@ def home(request):
     return HttpResponse(template.render(context))
 
 def list_form(request):
-    template = loader.get_template('form.html')
+    return render(request, "form.html")
 
+def list_form_update(request, rec_id):
+    data = get_object_or_404(List, pk=rec_id)
     context = {
-        
+        "id": data.id,
+        "text": data.text,
+        "due_date": data.due_date
     }
 
     return render(request, "form.html", context)
 
 def submit_form(request):
-    new_list = List(
-        text=request.POST["note"],
-        due_date=request.POST["due_date"],
-    )
-    new_list.save()
+    data = {
+        'id': request.POST['id'],
+        'text': request.POST["note"],
+        'due_date': request.POST["due_date"],
+    }
+    if request.POST['id']:
+        rec = get_object_or_404(List, pk=int(data["id"]))
+        rec.text = data["text"]
+        rec.due_date = data["due_date"]
+        rec.save()
+    else:
+        new_list = List(
+            text=data.text,
+            due_date=data.due_date,
+        )
+        new_list.save()
 
     return redirect('/list')
 
@@ -38,4 +53,8 @@ def delete_list(request, rec_id):
     data.delete()
     return redirect('/list')
 
-
+def record_is_done(request, rec_id):
+    data = get_object_or_404(List, pk=rec_id)
+    data.is_done = not data.is_done
+    data.save()
+    return redirect('/list')
