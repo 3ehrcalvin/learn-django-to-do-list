5c1d7c4e66e271cb4df216c90a15372f86e73554
3ehrcalvin <calvin@3ecpa.com.sg>
2025-02-05 17:43:45 +0800

    feat: add delete function

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("list/", views.home, name="list"),
     path("form/", views.list_form, name="form"),
     path("form/submit", views.submit_form, name="submit"),
+    path("delete/<int:rec_id>", views.delete_list, name="delete"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import List
-import uuid
-import datetime
 
 # Create your views here.
 
@@ -29,17 +27,15 @@ def list_form(request):
 def submit_form(request):
     new_list = List(
         text=request.POST["note"],
-        # due_date=datetime.datetime.strptime(request.POST['due_date'], '%Y-%m-%d').date(),
         due_date=request.POST["due_date"],
     )
     new_list.save()
 
     return redirect('/list')
-    # data = {
-    #     'id': uuid.uuid4,
-    #     'text': request.POST['note'],
-    #     'due_date': datetime.strptime(request.POST['due_date'], '%m/%d/%y %H:%M:%S'),
-    #     'is_done': False,
-    # }
+
+def delete_list(request, rec_id):
+    data = get_object_or_404(List, pk=rec_id)
+    data.delete()
+    return redirect('/list')
 
 
